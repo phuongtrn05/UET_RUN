40aa8c2b2227f4bbd4d42e429e194daf07538c40
DESKTOP-6ENBHPA\Chloe <tranthanhdat.tnn@gmail.com>
2025-10-06 21:58:55 +0700

    seventh: demo character

--- main.cpp
+++ main.cpp
@@ -13,6 +13,20 @@ enum class Scene {
     SCORE
 };
 
+// Cấu trúc cho nhân vật
+struct Player {
+    SDL_FRect rect;
+    float velocityX;
+    float velocityY;
+    bool isJumping;
+    bool onGround;
+    SDL_Color color;
+
+    Player() : rect{375, 400, 50, 60}, velocityX(0), velocityY(0),
+               isJumping(false), onGround(false),
+               color{255, 100, 100, 255} {}
+};
+
 SDL_Window* g_window = nullptr;
 SDL_Renderer* g_renderer = nullptr;
 TTF_Font* g_font = nullptr;
@@ -23,6 +37,14 @@ Uint8 g_alpha = 0;
 bool g_shrinking = false;
 Uint32 g_startTime = 0;
 Scene g_currentScene = Scene::MENU;
+Player g_player;
+
+// Các hằng số vật lý
+const float GRAVITY = 0.8f;
+const float JUMP_FORCE = -15.0f;
+const float MOVE_SPEED = 5.0f;
+const float GROUND_Y = 460.0f; // Vị trí mặt đất
+
 struct Button {
     std::string text;
     SDL_FRect rect;
@@ -66,14 +88,90 @@ bool checkCollision(float x, float y, const SDL_FRect& rect) {
             y >= rect.y && y < rect.y + rect.h);
 }
 
+// Hàm cập nhật vật lý cho nhân vật
+void updatePlayer() {
+    // Áp dụng trọng lực
+    g_player.velocityY += GRAVITY;
+
+    // Cập nhật vị trí
+    g_player.rect.x += g_player.velocityX;
+    g_player.rect.y += g_player.velocityY;
+
+    // Kiểm tra va chạm với mặt đất
+    if (g_player.rect.y + g_player.rect.h >= GROUND_Y) {
+        g_player.rect.y = GROUND_Y - g_player.rect.h;
+        g_player.velocityY = 0;
+        g_player.onGround = true;
+        g_player.isJumping = false;
+    } else {
+        g_player.onGround = false;
+    }
+
+    // Giới hạn trong màn hình
+    if (g_player.rect.x < 0) {
+        g_player.rect.x = 0;
+        g_player.velocityX = 0;
+    }
+    if (g_player.rect.x + g_player.rect.w > 800) {
+        g_player.rect.x = 800 - g_player.rect.w;
+        g_player.velocityX = 0;
+    }
+
+    // Giảm tốc độ di chuyển ngang (ma sát)
+    g_player.velocityX *= 0.9f;
+}
+
+// Hàm vẽ nhân vật
+void renderPlayer() {
+    // Vẽ thân nhân vật
+    SDL_SetRenderDrawColor(g_renderer, g_player.color.r, g_player.color.g, g_player.color.b, g_player.color.a);
+    SDL_RenderFillRect(g_renderer, &g_player.rect);
+
+    // Vẽ đầu nhân vật (hình tròn đơn giản hóa thành hình vuông)
+    SDL_FRect head = {
+        g_player.rect.x + 10,
+        g_player.rect.y - 20,
+        30,
+        30
+    };
+    SDL_SetRenderDrawColor(g_renderer, 255, 200, 150, 255); // Màu da
+    SDL_RenderFillRect(g_renderer, &head);
+
+    // Vẽ mắt
+    SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 255);
+    SDL_FRect leftEye = {head.x + 5, head.y + 8, 5, 5};
+    SDL_FRect rightEye = {head.x + 20, head.y + 8, 5, 5};
+    SDL_RenderFillRect(g_renderer, &leftEye);
+    SDL_RenderFillRect(g_renderer, &rightEye);
+}
+
 void renderScenePlay() {
-    SDL_SetRenderDrawColor(g_renderer, 100, 200, 100, 255);
+    // Vẽ bầu trời
+    SDL_SetRenderDrawColor(g_renderer, 135, 206, 250, 255); // Sky blue
     SDL_RenderClear(g_renderer);
+
+    // Vẽ mặt đất
+    SDL_FRect ground = {0, GROUND_Y, 800, 140};
+    SDL_SetRenderDrawColor(g_renderer, 34, 139, 34, 255); // Forest green
+    SDL_RenderFillRect(g_renderer, &ground);
+
+    // Vẽ một số cỏ
+    SDL_SetRenderDrawColor(g_renderer, 0, 100, 0, 255);
+    for (int i = 0; i < 800; i += 30) {
+        SDL_FRect grass = {(float)i, GROUND_Y - 10, 5, 10};
+        SDL_RenderFillRect(g_renderer, &grass);
+    }
+
+    // Cập nhật và vẽ nhân vật
+    updatePlayer();
+    renderPlayer();
+
+    // Hiển thị hướng dẫn
     SDL_Color textColor = {255, 255, 255, 255};
-    SDL_Surface* surface = TTF_RenderText_Blended(g_font, "Cảnh Play: Bấm ESC để về Menu", 0, textColor);
+    SDL_Surface* surface = TTF_RenderText_Blended(g_font, "A/D: Di chuyển, W/Space: Nhảy, ESC: Về Menu", 0, textColor);
     if (surface) {
         SDL_Texture* texture = SDL_CreateTextureFromSurface(g_renderer, surface);
-        SDL_FRect textRect = {50, 50, (float)surface->w, (float)surface->h};
+        SDL_FRect textRect = {10, 10, (float)surface->w * 0.5f, (float)surface->h * 0.5f};
         SDL_RenderTexture(g_renderer, texture, nullptr, &textRect);
         SDL_DestroySurface(surface);
         SDL_DestroyTexture(texture);
@@ -93,7 +191,6 @@ void renderSceneResume() {
         SDL_DestroyTexture(texture);
     }
 }
-
 
 void renderSceneScore() {
     SDL_SetRenderDrawColor(g_renderer, 255, 165, 0, 255);
@@ -152,6 +249,16 @@ void renderSceneMenu(Uint32 currentTime) {
     }
 }
 
+// Hàm reset nhân vật khi vào scene Play
+void resetPlayer() {
+    g_player.rect.x = 375;
+    g_player.rect.y = 400;
+    g_player.velocityX = 0;
+    g_player.velocityY = 0;
+    g_player.isJumping = false;
+    g_player.onGround = false;
+}
+
 int main(int argc, char* argv[]) {
     // Khởi tạo SDL và các thư viện
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -187,6 +294,7 @@ int main(int argc, char* argv[]) {
 
     bool running = true;
     SDL_Event e;
+    const bool* keystate = SDL_GetKeyboardState(nullptr);
 
     while (running) {
         Uint32 currentTime = SDL_GetTicks();
@@ -195,12 +303,22 @@ int main(int argc, char* argv[]) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) {
                 running = false;
-            } else if (e.type == SDL_EVENT_KEY_DOWN && e.key.key == SDLK_ESCAPE) {
-                // Quay về Menu khi bấm ESC ở các cảnh khác
-                if (g_currentScene != Scene::MENU) {
-                    g_currentScene = Scene::MENU;
-                } else {
-                    running = false;
+            } else if (e.type == SDL_EVENT_KEY_DOWN) {
+                if (e.key.key == SDLK_ESCAPE) {
+                    // Quay về Menu khi bấm ESC ở các cảnh khác
+                    if (g_currentScene != Scene::MENU) {
+                        g_currentScene = Scene::MENU;
+                    } else {
+                        running = false;
+                    }
+                }
+                // Xử lý nhảy trong scene Play
+                else if (g_currentScene == Scene::PLAY) {
+                    if ((e.key.key == SDLK_W || e.key.key == SDLK_SPACE) && g_player.onGround) {
+                        g_player.velocityY = JUMP_FORCE;
+                        g_player.isJumping = true;
+                        g_player.onGround = false;
+                    }
                 }
             } else if (e.type == SDL_EVENT_MOUSE_BUTTON_DOWN && e.button.button == SDL_BUTTON_LEFT) {
                 // Xử lý click chuột
@@ -210,6 +328,7 @@ int main(int argc, char* argv[]) {
                     // Kiểm tra nút Play
                     if (checkCollision(mouseX, mouseY, g_buttons[0].rect)) {
                         g_currentScene = Scene::PLAY;
+                        resetPlayer(); // Reset vị trí nhân vật
                     }
                     // Kiểm tra nút Resume
                     else if (checkCollision(mouseX, mouseY, g_buttons[1].rect)) {
@@ -220,6 +339,16 @@ int main(int argc, char* argv[]) {
                         g_currentScene = Scene::SCORE;
                     }
                 }
+            }
+        }
+
+        // Xử lý di chuyển liên tục trong scene Play
+        if (g_currentScene == Scene::PLAY) {
+            if (keystate[SDL_SCANCODE_A]) {
+                g_player.velocityX = -MOVE_SPEED;
+            }
+            if (keystate[SDL_SCANCODE_D]) {
+                g_player.velocityX = MOVE_SPEED;
             }
         }
 
